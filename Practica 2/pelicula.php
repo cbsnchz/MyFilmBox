c709c583c99e7f37356287d079e37a7f23af5363
evaverdu <43961063+evaverdu@users.noreply.github.com>
2020-03-23 19:04:20 +0100

    Pelicula

--- Practica 2/pelicula.php
+++ Practica 2/pelicula.php
@@ -1,1 +1,18 @@
-ñalkghle
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+	<link rel="stylesheet" type="text/css" href="css/peliculacss.css" />
+	<meta charset="utf-8">
+	<title>Peliculas</title>
+</head>
+
+<form class="formulario">
+	<div class ="contenedor">
+		<h1> Regreso al futuro <h1>
+		<img class = "img_peli" src= "img/regresoalfuturo.jpg">
+			
+	</div>	
+ 
+ </html>
