4f14cf940a4b95a52f671c1b631bddd1bb1a8460
evaverdu <43961063+evaverdu@users.noreply.github.com>
2020-03-24 23:21:47 +0100

    2

--- Practica 2/pelicula.php
+++ Practica 2/pelicula.php
@@ -8,18 +8,34 @@
 	<title>Peliculas</title>
 </head>
 
-<form class="formulario">
+<body>
+ <?php
+	include('includes/common/cabecera.php');
+ ?>
+
+<form class="formulario">	
 	<div class ="contenedor">
-		<h1> Regreso al futuro <h1>
+		<h2> Regreso al futuro <h2>
 		<img class = "img_peli" src= "img/regresoalfuturo.jpg">
-			<p> Año: 1965 </p>
-			<p> Duración: 116 min.</p>
-			<p> País: Estados Unidos </p>
-			<p> Director: Robert Zemeckis </p>
-			<p> Reparto: Michael J. Fox, Christopher Lloyd, Lea Thompson, Crispin Glover, Claudia Wells, Thomas F. Wilson, James Tolkan, Billy Zane, Sachi Parker </p>
-			<p> Productora: Universal Pictires/ Amblin Entertainment</p>
-			<p> Genero: Ciencia Ficcion </p>
-			<p> Sinopsis: El adolescente Marty McFly es amigo de Doc, un científico al que todos toman por loco. Cuando Doc crea una máquina para viajar en el tiempo, un error fortuito hace que Marty llegue a 1955, año en el que sus futuros padres aún no se habían conocido. Después de impedir su primer encuentro, deberá conseguir que se conozcan y se casen; de lo contrario, su existencia no sería posible. </p>
-	</div>	
- 
+		<p> Año: 1965 </p>
+		<p> Duración: 116 min.</p>
+		<p> País: Estados Unidos </p>
+		<p> Director: Robert Zemeckis </p>
+		<p> Reparto: Michael J. Fox, Christopher Lloyd, Lea Thompson, Crispin Glover, Claudia Wells, Thomas F. Wilson, James Tolkan, Billy Zane, Sachi Parker </p>
+		<p> Productora: Universal Pictires/ Amblin Entertainment</p>
+		<p> Genero: Ciencia Ficcion </p>
+		<p> Sinopsis: El adolescente Marty McFly es amigo de Doc, un científico al que todos toman por loco. Cuando Doc crea una máquina para viajar en el tiempo, un error fortuito hace que Marty llegue a 1955, año en el que sus futuros padres aún no se habían conocido. Después de impedir su primer encuentro, deberá conseguir que se conozcan y se casen; de lo contrario, su existencia no sería posible. </p>
+	</div>
+</form>	
+<div class = "formulario">
+		<div class = "contenedor">
+		<h2> Comentarios </h2>
+		<p> esta muy chachi</p>
+		</div>
+</div>
+
+ </body>
+ <?php	
+	include('includes/common/pie.php');
+?>
  </html>
