d4f1098a33b63f1095bd63f7dce6759485949649
evaverdu <43961063+evaverdu@users.noreply.github.com>
2020-03-24 16:11:01 +0100

    mas cosos

--- Practica 2/pelicula.php
+++ Practica 2/pelicula.php
@@ -12,7 +12,14 @@
 	<div class ="contenedor">
 		<h1> Regreso al futuro <h1>
 		<img class = "img_peli" src= "img/regresoalfuturo.jpg">
-			
+			<p> Año: 1965 </p>
+			<p> Duración: 116 min.</p>
+			<p> País: Estados Unidos </p>
+			<p> Director: Robert Zemeckis </p>
+			<p> Reparto: Michael J. Fox, Christopher Lloyd, Lea Thompson, Crispin Glover, Claudia Wells, Thomas F. Wilson, James Tolkan, Billy Zane, Sachi Parker </p>
+			<p> Productora: Universal Pictires/ Amblin Entertainment</p>
+			<p> Genero: Ciencia Ficcion </p>
+			<p> Sinopsis: El adolescente Marty McFly es amigo de Doc, un científico al que todos toman por loco. Cuando Doc crea una máquina para viajar en el tiempo, un error fortuito hace que Marty llegue a 1955, año en el que sus futuros padres aún no se habían conocido. Después de impedir su primer encuentro, deberá conseguir que se conozcan y se casen; de lo contrario, su existencia no sería posible. </p>
 	</div>	
  
  </html>
