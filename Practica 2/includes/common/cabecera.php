94f59227c8679f25c460777946dc00a39eec1229
cbsnchz <60937695+cbsnchz@users.noreply.github.com>
2020-03-29 18:49:30 +0200

    CSS BUSQUEDA Y USUARIO

--- Practica 2/includes/common/cabecera.php
+++ Practica 2/includes/common/cabecera.php
@@ -1,31 +1,41 @@
 <!DOCTYPE html>
 <html>
 <link rel="stylesheet" type="text/css" href="css/cabeceracss.css" />
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
 <div id="cabecera">
 		<h1>MyFilmBox</h1>
 		<body>
+		
 		<ul>
 			<li><a class="active" href="index.php">Home</a></li>
 			<li><a href="catalogo.php">Catálogo</a></li>
 			<li><a href="tienda.php">Tienda</a></li>
-			<div class="search-container">
+
+
+			
+
+			 
+			 <?php
+		
+		if(!isset($_SESSION["login"])){
+			$html = '<li id="boton" style="float:right"><a href="login.php"><i class="far fa-user icon-login"></i></a></li>';
+		}
+		else{
+			$html = '<li id="boton" style="float:right"><a href="logout.php">' . $_SESSION["nombre"] . '</a></li>';
+		}
+		echo $html; 
+		?>	
+
+
+		<li style="float:right">
+		<div  class="container-3">
 				<form action="buscar.php" method="post">
-				  <input type="text" placeholder="Search.." name="search">
-				  <button type="submit">Submit</button>
+				 <span class="icon"><i class="fa fa-search"></i></span>
+				  <input type="search" id="search" placeholder="Search.." name="search">
+				  
 				</form>
 			 </div>
-			
-			<?php
-		
-			if(!isset($_SESSION["login"])){
-				echo "<li id=\"boton\" style=\"float: right\"><a href=\"login.php\">Login</a></li>";
-			}
-			else{
-				//echo "<li id=\"boton\" style=\"float: right\"><a href=\"cuenta.php\">". $_SESSION["nombre"] ."</li>";
-				$html = '<li id="boton" style="float:right"><a href="logout.php">' . $_SESSION["nombre"] . '</a></li>';
-				echo $html; 
-			}
-			?>		
+		</li>
 			
 		</ul>
 		</body>
