122b2cdf118ec810fcf465842d8db0e8ad8cb2aa
jalguacilgon <43850115+jalguacilgon@users.noreply.github.com>
2020-05-01 20:42:34 +0200

    BarraTitulo.css

--- Practica 3/includes/common/barraTitulo.php
+++ Practica 3/includes/common/barraTitulo.php
@@ -4,6 +4,16 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
 <div id="titlebar">
     <div id="tittle"> My FilmBox </div>
+
+
+    <div  class="container-3">
+        <form action="buscar.php" method="post">
+            <span class="icon"><i class="fa fa-search"></i></span>
+            <input type="search" id="search" placeholder="Buscar..." name="search">
+            
+        </form>
+    </div>
+
     
     <div id="access">
         <p> <a class="l+r" href="login.php"> Login </a> <a class="l+r" href="registro.php"> Registrarse </a></p>
