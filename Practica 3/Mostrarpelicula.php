c8bc37dce04d09a94e3175fa1e6ecdf5ae7466d7
evaverdu <43961063+evaverdu@users.noreply.github.com>
2020-05-05 22:07:43 +0200

    comentarios css

--- Practica 3/Mostrarpelicula.php
+++ Practica 3/Mostrarpelicula.php
@@ -29,11 +29,46 @@ include_once('includes/Comentarios.php');
 <div class = "formulario">
 		<div class = "contenedor">
 		<h2> Comentarios </h2>
+		<div class="comments-container">
+		<ul id="comments-list" class="comments-list">
 		<?php
 			$id = $_GET["id"];
-			$c = new es\ucm\fdi\aw\Comentarios();
-			$c->imprimeComentarios($id);
+			$c = new es\ucm\fdi\aw\Comentarios(null,null,null,null);
+			$comentarios = $c->imprimeComentarios($id);
+			foreach ($comentarios as &$value) {
+				$html = '
+				<html>
+				<head>
+                            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+                            <link rel="stylesheet" type="text/css" href="css/comentarioscss.css" />
+                            <meta charset="utf-8">
+                            <title>Comentarios</title>
+                        </head>
+				<body>
+					<li>
+						<div class="comment-main-level">
+							<div class="comment-avatar"><img src="img/img_avatar.png" alt=""></div>
+							<div class="comment-box">
+						<div class="comment-head">
+							<h6 class="comment-name"><a href="http://creaticode.com/blog">'.$value->getUsuario().'</a></h6>
+							<span>
+							"'.$value->getTitulo().'"</span>
+							<i class="fa fa-reply"></i>
+							<i class="fa fa-heart"></i>
+						</div>
+						<div class="comment-content">'.$value->getTexto().'</div>
+					</div>
+				</div>
+				</body>
+				</html>';
+				echo $html;
+				/**echo "<h3>".$value->getTitulo()." por: ".$value->getUsuario()." fecha: ".$value->getFecha()."<h3>";
+				echo "<p>".$value->getTexto()."<p>";*/
+			}
+    
 		?>
+		</ul>
+		</div>
 		</div>
 </div>
 
--- Practica 3/includes/Comentarios.php
+++ Practica 3/includes/Comentarios.php
@@ -3,19 +3,52 @@ namespace es\ucm\fdi\aw;
 
 class Comentarios{
 	
+	private $titulo;
+	private $usuario;
+	private $texto;
+	private $fecha;
+	
+	public function __construct($_titulo, $_usuario, $_fecha, $_texto){
+		$this->titulo = $_titulo;
+		$this->usuario = $_usuario;
+		$this->fecha = $_fecha;
+		$this->texto = $_texto;
+	}
+	
 	public function imprimeComentarios($id){
 		$app = AplicacionPeliculas::getSingleton();
 		$conn = $app->conexionBd();
 		$sql = "SELECT * FROM comentarios WHERE id_pelicula = '$id'";
 		$result = $conn->query($sql)
 			   or die ($conn->error. " en la línea ".(__LINE__-1));
+		$comentarios = array();
 		if($result->num_rows > 0){
+			$i = 0;
 			while($fila = $result->fetch_assoc()){
-				echo "<h3>".$fila["titulo"]." por: ".$fila["usuario"]." fecha: ".$fila["Fecha"]."<h3>";
-				echo "<p>".$fila["texto"]."<p>";
+				$comentarios[$i] = new Comentarios($fila["titulo"],$fila["usuario"],$fila["Fecha"],$fila["texto"]);
+				$i = $i+1;
 			}
 		}
-			
+		return $comentarios;	
 	}
+	
+	public function getTitulo(){
+		return $this->titulo;
+	}
+	
+	public function getUsuario(){
+		return $this->usuario;
+	}
+	
+	public function getFecha(){
+		return $this->fecha;
+	}
+	
+	public function getTexto(){
+		return $this->texto;
+	}
+	
+	
+	
 	
 }
