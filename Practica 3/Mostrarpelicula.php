22a978408044774a39a807c1ffde5a079f79ac11
evaverdu <43961063+evaverdu@users.noreply.github.com>
2020-05-07 17:50:29 +0200

    mostrar peliculas

--- Practica 3/Mostrarpelicula.php
+++ Practica 3/Mostrarpelicula.php
@@ -21,8 +21,25 @@ include_once('includes/Comentarios.php');
 	<div class ="contenedor">
 		<?php
 			$id = $_GET["id"];
-			$p = new es\ucm\fdi\aw\Pelicula();
-			$p->imprimePelicula($id);
+			$app = es\ucm\fdi\aw\AplicacionPeliculas::getSingleton();
+			$conn = $app->conexionBd();
+			$sql = "SELECT * FROM pelicula WHERE id = '$id'";
+			$result = $conn->query($sql)
+			   or die ($conn->error. " en la línea ".(__LINE__-1));
+
+		if($result->num_rows > 0){
+			$fila = $result->fetch_assoc();
+			echo "<h2>".$fila["nombre"]."<h2>";
+			echo '<img class = "img_peli" src="'.$fila["imagen"].'">';
+			echo "<p> Año: ".$fila["anyo"]."<p>";
+			echo "<p> Duración: ".$fila["duracion"]." min <p>";
+			echo "<p> Director: ".$fila["director"]."<p>";
+			echo "<p> Reparto: ".$fila["reparto"]."<p>";
+			echo "<p> Productora: ".$fila["productora"]."<p>";
+			echo "<p> Genero: ".$fila["genero"]."<p>";
+			echo "<p> Sinopsis: ".$fila["sinopsis"]."<p>";
+			
+		}
 		?>
 	</div>
 </form>	
