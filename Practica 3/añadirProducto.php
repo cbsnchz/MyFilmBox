2a5f367d0a6d126d526c51ff5ac16a7aa47a557b
teresitaFerme <60939567+teresitaFerme@users.noreply.github.com>
2020-05-08 01:34:52 +0200

    Añadir producto arreglado
    Implementada funcionalidad total de añadir productos a la tienda

---
+++ Practica 3/añadirProducto.php
@@ -0,0 +1,36 @@
+
+<?php
+require_once __DIR__.'/includes/config.php';
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>Añadir Producto</title>
+	</head>
+
+	<body>
+		<div id="contenedor">
+			
+			<?php
+			
+				require("includes/common/cabecera.php");
+			?>
+
+			<div id="contenido">
+				
+				<?php 
+					
+					$form = new es\ucm\fdi\aw\FormularioRegistroProducto(); $form->gestiona();
+				?>
+			</div>
+
+			<?php
+				require("includes/common/pie.php");
+			?>
+
+
+		</div>
+	</body>
+</html>
---
+++ Practica 3/includes/producto.php
@@ -0,0 +1,197 @@
+<?php
+namespace es\ucm\fdi\aw;
+
+class Producto{
+	
+	public function imprimeProducto($id){
+		$app = AplicacionProductos::getSingleton();
+		$conn = $app->conexionBd();
+		$sql = "SELECT * FROM producto WHERE Id = '$id'";
+		$result = $conn->query($sql)
+			   or die ($conn->error. " en la línea ".(__LINE__-1));
+
+		if($result->num_rows > 0){
+			$fila = $result->fetch_assoc();
+			echo "<h2>".$fila["nombre"]."<h2>";
+			echo '<img class = "img_peli" src="'.$fila["imagen"].'">';
+			echo "<p> Año: ".$fila["anyo"]."<p>";
+			echo "<p> Duración: ".$fila["duracion"]." min <p>";
+			echo "<p> Director: ".$fila["director"]."<p>";
+			echo "<p> Reparto: ".$fila["reparto"]."<p>";
+			echo "<p> Productora: ".$fila["productora"]."<p>";
+			echo "<p> Genero: ".$fila["genero"]."<p>";
+			echo "<p> Sinopsis: ".$fila["sinopsis"]."<p>";
+			
+		}
+	}
+	
+	public static function buscaProducto($nombre)
+    {
+        $app = AplicacionProductos::getSingleton();
+        $conn = $app->conexionBd();
+       
+        $query = sprintf("SELECT * FROM producto U WHERE U.nombre = '%s'", $conn->real_escape_string($nombre));
+        $rs = $conn->query($query);
+        $result = false;
+        if ($rs) {
+            if ( $rs->num_rows == 1) {
+                $fila = $rs->fetch_assoc();
+                $Producto = new Producto($fila['nombre'], $fila['precio'], $fila['descripcion'], $fila['categoria'], $fila['imagen']);
+                $Producto->Id = $fila['Id'];
+                $result = $Producto;
+            }
+            $rs->free();
+        } else {
+            echo "Error al consultar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+            exit();
+        }
+        return $result;
+    }
+    
+    public static function crea($nombre, $precio, $descripcion, $categoria, $imagen)
+    {
+        $Producto = self::buscaProducto($nombre);
+        if ($Producto) {
+            return false;
+        }
+        $Producto = new Producto($nombre, $precio, $descripcion, $categoria, $imagen);
+        return self::inserta($Producto);
+    }
+    
+    private static function inserta($Producto)
+    {
+        $app = AplicacionProductos::getSingleton();
+        $conn = $app->conexionBd();
+        $query=sprintf("INSERT INTO producto(nombre, precio, descripcion, categoria, imagen) VALUES('%s', '%s', '%s', '%s','%s')"
+            , $conn->real_escape_string($Producto->nombre)
+            , $conn->real_escape_string($Producto->precio)
+            , $conn->real_escape_string($Producto->descripcion)
+            , $conn->real_escape_string($Producto->categoria)
+			, $conn->real_escape_string($Producto->imagen));        
+        if ( $conn->query($query) ) {
+            $Producto->Id = $conn->insert_id;
+        } else {
+            echo "Error al insertar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+            exit();
+        }
+        return $Producto;
+    }
+    
+  
+	private $Id;
+	private $nombre;
+	private $precio;
+	private $descripcion;
+	private $categoria;
+	private $imagen;
+
+    private function __construct($nombre, $precio, $descripcion, $categoria, $imagen)
+    {
+        $this->nombre= $nombre;
+        $this->precio = $precio;
+        $this->descripcion = $descripcion;
+        $this->categoria = $categoria;
+		$this->imagen = $imagen;
+    }
+	public function Id()
+    {
+        return $this->Id;
+    }
+	public function imagen()
+    {
+        return $this->imagen;
+    }
+    public function nombre()
+    {
+        return $this->nombre;
+    }
+	public function precio()
+    {
+        return $this->precio;
+    }
+	public function descripcion()
+    {
+        return $this->descripcion;
+    }
+	public function categoria()
+    {
+        return $this->categoria;
+    }
+	
+	 public static function imprimeListaPeliculas()
+    {
+        $app = AplicacionPeliculas::getSingleton();
+        $conn = $app->conexionBd();
+        $sql = "SELECT * FROM Pelicula p";
+        $result = $conn->query($sql);
+        if ($result) {
+            if ( $result->num_rows > 0) {
+                $html = '
+                    <html>
+                        <head>
+                            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+							<link rel="stylesheet" type="text/css" href="css/catalogo.css" />                            <meta charset="utf-8">
+                            
+                        </head>
+						
+						<div class = "contenedor">
+                        <body>
+                            <?php
+                                include("includes/common/cabecera.php");
+                            ?>
+                        <div class="contenido" class="main">
+							<div id="myBtnContainer">
+							 <button class="btn active" onclick="filterSelection(\'all \')"> Todos</button>
+							  <button class="btn" onclick="filterSelection(\'Accion \')"> Accion</button>
+							  <button class="btn" onclick="filterSelection(\'Adultas \')"> Adultas</button>
+							  <button class="btn" onclick="filterSelection(\'Aventuras \')"> Aveturas</button>
+							  <button class="btn" onclick="filterSelection(\'Belica \')"> Bélica</button>
+							  <button class="btn" onclick="filterSelection(\'Ciencia Ficcion \')"> Ciencia Ficcion</button>
+							  <button class="btn" onclick="filterSelection(\'Comedia \')"> Comedia</button>
+							  <button class="btn" onclick="filterSelection(\'Drama \')"> Drama</button>
+							  <button class="btn" onclick="filterSelection(\'Infantiles \')"> Infantiles</button>
+							  <button class="btn" onclick="filterSelection(\'Musical \')"> Musical</button>
+							  <button class="btn" onclick="filterSelection(\'Musical \')"> Oeste</button>
+							  <button class="btn" onclick="filterSelection(\'Romance \')"> Romance</button>
+							  <button class="btn" onclick="filterSelection(\'Terror \')"> Terror</button>
+							  <button class="btn" onclick="filterSelection(\'Thiller \')"> Thiller</button>
+		  
+							</div>
+							
+							';
+
+                while($fila = $result->fetch_assoc()){
+                   $html .= '
+				   <div class="column '.$fila["genero"].'">
+								<div class="content">
+									<div class = "card-peli">
+										<img src="'.$fila["imagen"].'"style="width:100%">
+										<div class = "container-card">
+											<h4> <a href="Mostrarpelicula.php?id="'.$fila["id"].'">"'.$fila["nombre"].'"</a> </h4>
+											<h5> '.$fila["director"].'</h5>
+										</div>
+									</div>
+								</div>
+							</div>';
+                                 
+                                
+                }
+                $html.= '
+                    </div>
+                    </body>
+                    <?php	
+                        include("includes/common/pie.php");
+                    ?>
+					</div>
+					<script type="text/javascript" src="js/catalogo.js"></script>
+                    </html>';
+
+            }
+            $result->free();
+        } else {
+            echo "Error al consultar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+            exit();
+        }
+        echo $html;
+    }
+}
