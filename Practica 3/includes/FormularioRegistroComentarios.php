7ac641d7e94f799eb184ade872595e622f2919d6
evaverdu <43961063+evaverdu@users.noreply.github.com>
2020-05-08 20:30:23 +0200

    mostrar peliculas mejor

--- Practica 3/includes/FormularioRegistroComentarios.php
+++ Practica 3/includes/FormularioRegistroComentarios.php
@@ -3,11 +3,13 @@ namespace es\ucm\fdi\aw;
 
 class FormularioRegistroComentarios extends Form
 {
-	private $id;
+	private $num = 0;
 	
     public function __construct($_id) {
-        parent::__construct('formRegistroComentarios');
-		$this->id = $_id;
+		$head = "MostrarPelicula.php?id=".$_id;
+		$opcionesPorDefecto = array( 'action' => $head );
+        parent::__construct('formRegistroComentarios', $opcionesPorDefecto);
+		$this->num = $_id;
     }
     
     protected function generaCamposFormulario($datos)
@@ -26,17 +28,21 @@ class FormularioRegistroComentarios extends Form
         $html = file_get_contents("includes/ViewRegistroComentarios.php");
         return $html;
     }
-    
+   
 
     protected function procesaFormulario($datos)
     {
 		$titulo = isset($datos['titulo']) ? $datos['titulo'] : null;
 		$usuario = isset($datos['usuario']) ? $datos['usuario'] : null;
 		$comentario = isset($datos['comentario']) ? $datos['comentario'] : null;
-		
+        if($this->num != 0){
+			$Comentarios = Comentarios::crea($titulo, $usuario, $comentario, $this->num);
+			if(!$Comentarios) echo 'No se ha podido crear el comentario';
+			return "MostrarPelicula.php?id=".$this->num;
+		}
+		else{
+			return 'catalogo.php';
+		}
         
-		$Comentarios = Comentarios::crea($titulo, $usuario, $comentario, $this->id);
-        if(!$Comentarios) echo 'No se ha podido crear el comentario';
-        return 'index.php';
     }
 }
