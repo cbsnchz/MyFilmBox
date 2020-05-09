6b1581cbdfe8469c0d4d84aceeade8025929a9d2
davidferpe99 <60969608+davidferpe99@users.noreply.github.com>
2020-05-09 17:25:34 +0200

    añadido grid layout + buscar.css

--- Practica 3/buscar.php
+++ Practica 3/buscar.php
@@ -2,7 +2,12 @@
 <html>
 <head>
 		<?php include_once('includes/config.php'); ?>
-		<link rel="stylesheet" type="text/css" href="css/catalogo.css" />
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" type="text/css" href="css/buscar.css" />
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+		<meta charset="utf-8">
+
 </head>
 <body>
 	<div id="contenedor">
@@ -12,49 +17,7 @@
 		
 		<div id="contenido" class="main">			
 			<?php
-			$conn = new mysqli(BD_HOST, BD_USER, BD_PASS, BD_NAME_PELI);
-			if ($conn->connect_error) {
-			   die("Fallo de conexion con la base de datos: " . $conn->connect_error);
-			}
-			else{
-				$conn->set_charset("utf8");
-				$busqueda = $_POST['search'];
-				$sql = "SELECT id, nombre, imagen, genero FROM pelicula WHERE nombre LIKE '%$busqueda%'";
-				$result = $conn->query($sql)
-					   or die ($conn->error. " en la línea ".(LINE-1));
-
-				if($result->num_rows > 0){
-					while($fila = $result->fetch_assoc()){
-						echo '<div class="column '.$fila["genero"].'">';
-							echo '<div class="content">';
-								echo '<img src="'.$fila["imagen"].'"style="width:20%">';
-								echo "<h4> <a href=\"pelicula.php?id=".$fila["id"]."\">".$fila["nombre"]."</a> </h4>";
-							echo '</div>';
-						echo '</div>';
-					}
-				}
-				else{
-					echo '<h2>No se han encontrado resultados para la búsqueda: '.$busqueda.'.</h2>';
-					while($result->num_rows === 0){
-						$busqueda = substr($busqueda, 0, -1);
-						
-						$sql = "SELECT id, nombre, imagen, genero FROM pelicula WHERE nombre LIKE '%$busqueda%'";
-						$result = $conn->query($sql)
-							   or die ($conn->error. " en la línea ".(LINE-1));
-					}
-					echo '<h2>Resultados relacionados: </h2>';
-					while($fila = $result->fetch_assoc()){
-						echo '<div class="column '.$fila["genero"].'">';
-							echo '<div class="content">';
-								echo '<img src="'.$fila["imagen"].'"style="width:20%">';
-								echo "<h4> <a href=\"pelicula.php?id=".$fila["id"]."\">".$fila["nombre"]."</a> </h4>";
-							echo '</div>';
-						echo '</div>';
-					}
-					
-				}
-			}
-			$conn -> close();
+				es\ucm\fdi\aw\Pelicula::buscarPeliculas();
 			?>		
 		</div>
 		
